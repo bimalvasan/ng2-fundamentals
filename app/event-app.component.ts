0e76ea4f85f5b65ce5dfb7570074f86eb01df683
bimalvasan <bimalvasan@gmail.com>
2017-03-01 06:30:42 +0530

    data binding included

--- app/event-app.component.ts
+++ app/event-app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'event-app',
-    template: '<h2>Hello World!</h2>'
+    //template: '<h2>Hello World!</h2>'
+    template: '<events-list></events-list>'
 })
 export class EventsAppComponent {
 
