8f82d227a5d18202a8545ad8776b320310457491
bimalvasan <bimalvasan@gmail.com>
2017-03-07 06:50:03 +0530

    Hiding and showing with ngIf

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     selector: 'event-thumbnail',
     templateUrl: 'app/events/event-thumbnail.component.html',
     styles: [
-        '.pad-left { margin-left: 50px; }' 
+        '.pad-left { margin-left: 10px; } .well div { color: #bbb; } .thumbnail { min-height: 210px; }' 
     ]
 })
 export class EventThumbnailComponent{
