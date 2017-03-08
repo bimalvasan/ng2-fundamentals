2e176f6f47c57824eaa2f4a155102b27d53a0ad4
bimalvasan <bimalvasan@gmail.com>
2017-03-08 21:24:53 +0530

    ngClass for styles

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,10 +4,21 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     selector: 'event-thumbnail',
     templateUrl: 'app/events/event-thumbnail.component.html',
     styles: [
-        '.green { color: #003300 !important; } .bold { font-weight: bold; } .pad-left { margin-left: 10px; } .well div { color: #bbb; } .thumbnail { min-height: 210px; }' 
+        '.green { color: #003300 !important; } .bold { font-weight: bold; } .pad-left { margin-left: 10px; } .well div { color: #bbb; } .thumbnail { min-height: 210px; }'
     ]
 })
-export class EventThumbnailComponent{
-   @Input() event: any
-   @Output() eventClick = new EventEmitter()
+export class EventThumbnailComponent {
+    @Input() event: any
+    @Output() eventClick = new EventEmitter()
+
+    getStartTimeClass() {
+        const isEarlyStart = this.event && this.event.time === '8:00 am'
+
+        //{ green: event?.time === '8:00 am', bold: event?.time === '8:00 am'}
+
+        return {
+            green: isEarlyStart,
+            bold: isEarlyStart
+        }
+    }
 }
