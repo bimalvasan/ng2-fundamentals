dc1a387c1869df9ef44e29a2a9f43c159d0d81da
bimalvasan <bimalvasan@gmail.com>
2017-03-09 19:30:45 +0530

    Dependency Injection

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
 
 @Component({
     selector: 'events-list',
     templateUrl: 'app/events/events-list.component.html'
 })
-export class EventListComponent {
-    events: any[];
-    constructor(private eventService: EventService) {
-        this.events = eventService.getEvents();
+export class EventListComponent implements OnInit {
+    events: any[]
+    eventService: EventService
+    
+    constructor(private eventSvc: EventService) {
+        this.eventService = eventSvc
+    }
+
+    ngOnInit() {
+        this.events = this.eventService.getEvents()
     }
 }
