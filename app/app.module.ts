43e81ea11609d389ed8304e6d5ee4786cb0ba264
Bimal <bimalvasan@gmail.com>
2017-04-02 18:47:06 +0530

    Guarding against route activation

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,16 +8,17 @@ import { EventListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { CreateEventComponent } from './events/create-event.component'
-
 import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
+import { Error404Component } from './errors/404.component'
+import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 
 import { appRoutes } from './routes'
 
 @NgModule({
     imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-    declarations: [NavbarComponent, EventsAppComponent, EventListComponent, EventThumbnailComponent, EventDetailsComponent, CreateEventComponent],
-    providers: [EventService, ToastrService],
+    declarations: [NavbarComponent, EventsAppComponent, EventListComponent, EventThumbnailComponent, EventDetailsComponent, CreateEventComponent, Error404Component],
+    providers: [EventService, ToastrService, EventRouteActivator],
     bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
